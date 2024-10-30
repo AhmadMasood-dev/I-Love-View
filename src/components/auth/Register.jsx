04c11a94259453120499f505984aa3889c0c4ce3
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-10-30 13:40:59 +0500

    make the spinner its active when it verify the email

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -11,6 +11,8 @@ import {
   verify,
 } from "../../assets/images/images.js";
 
+import Loader from "../../ui/Spinner.jsx";
+
 const Login = () => {
   const [formData, setFormData] = useState({
     fullName: "",
@@ -20,11 +22,17 @@ const Login = () => {
     terms: false,
   });
 
-  const [verifyEmail, setVerifyEmail] = useState(false);
+  const [verifyEmail, setVerifyEmail] = useState(true);
   const [errors, setErrors] = useState({});
   const [submitError, setSubmitError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  // verify email
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const correctCode = "123456";
 
   // Toggles
   const togglePassword = () => setShowPassword(!showPassword);
@@ -43,6 +51,7 @@ const Login = () => {
   // Form validation and submission handler
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
 
     const newErrors = {};
     if (!formData.fullName) newErrors.fullName = "Full Name is required.";
@@ -58,18 +67,36 @@ const Login = () => {
     setErrors(newErrors);
 
     if (Object.keys(newErrors).length === 0) {
-      setSubmitError("");
-      setFormData({
-        fullName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-        terms: false,
-      });
-      alert("Form submitted successfully!");
+      setTimeout(() => {
+        setSubmitError("");
+        setFormData({
+          fullName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+          terms: false,
+        });
+        setVerifyEmail(false);
+        setLoading(false); // Stop loading
+      }, 1500);
     } else {
       setSubmitError("Some went Wrong..");
+      setLoading(false);
     }
+  };
+
+  const handleVerify = () => {
+    setLoading(true); // Start loading
+
+    // Simulate server verification delay (2 seconds)
+    setTimeout(() => {
+      if (code === correctCode) {
+        setMessage("✅ Code verified successfully!");
+      } else {
+        setMessage("❌ Invalid code, please try again.");
+      }
+      setLoading(false); // Stop loading
+    }, 2000);
   };
 
   return (
@@ -82,181 +109,204 @@ const Login = () => {
 
       {/* Right Side with Form */}
       <div className="w-full sm:w-1/2 flex items-center justify-center  bgImage px-5">
-        <div className="w-full sm:w-[644px] sm:h-[885px]  sm:shadow-xl rounded-lg flex items-center justify-center flex-col">
-          <img
-            src={login}
-            alt=""
-            className="w-[213px] sm:w-[344px] sm:h-[263px] h-[163px]"
-          />
-          <h2 className="text-xl font-semibold mb-1 text-center">
-            Create Your iLoveView Account
-          </h2>
-          <p className="text-secondary text-center mb-6 text-sm font-normal">
-            Sign up to send and receive private videos with family and friends.
-          </p>
+        {!loading ? (
+          <div className="w-full sm:w-[644px] sm:h-[885px]  sm:shadow-xl rounded-lg flex items-center justify-center flex-col">
+            <>
+              <img
+                src={login}
+                alt=""
+                className="w-[213px] sm:w-[344px] sm:h-[263px] h-[163px]"
+              />
+              <h2 className="text-xl font-semibold mb-1 text-center">
+                Create Your iLoveView Account
+              </h2>
+              <p className="text-secondary text-center mb-6 text-sm font-normal">
+                Sign up to send and receive private videos with family and
+                friends.
+              </p>
 
-          <form className="space-y-4 max-w-md mx-auto" onSubmit={handleSubmit}>
-            {/* Full Name */}
+              <form
+                className="space-y-4 max-w-md mx-auto"
+                onSubmit={verifyEmail ? handleSubmit : handleVerify}
+              >
+                {/* Full Name */}
 
-            {verifyEmail ? (
-              <>
-                <div className="relative ">
-                  <div className=" absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <img src={user} className=" text-secondary items-center" />
-                  </div>
+                {verifyEmail ? (
+                  <>
+                    <div className="relative ">
+                      <div className=" absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                        <img
+                          src={user}
+                          className=" text-secondary items-center"
+                        />
+                      </div>
 
-                  <input
-                    // className=" border text-sm rounded-2xl shadow-md  block w-full ps-10 p-2.5  "
-                    type="text"
-                    name="fullName"
-                    className={`border text-sm rounded-2xl shadow-md block w-full ps-10 p-2.5  ${
-                      errors.fullName ? "border-red-500" : ""
-                    }`}
-                    placeholder="Full Name"
-                    value={formData.fullName}
-                    onChange={handleInputChange}
-                  />
-                </div>
+                      <input
+                        // className=" border text-sm rounded-2xl shadow-md  block w-full ps-10 p-2.5  "
+                        type="text"
+                        name="fullName"
+                        className={`border text-sm rounded-2xl shadow-md block w-full ps-10 p-2.5  ${
+                          errors.fullName ? "border-red-500" : ""
+                        }`}
+                        placeholder="Full Name"
+                        value={formData.fullName}
+                        onChange={handleInputChange}
+                      />
+                    </div>
 
-                {/* Email */}
-                <div className="relative">
-                  <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <img src={email} className=" text-secondary items-center" />
-                  </div>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className={` border text-sm rounded-2xl shadow-md  block w-full ps-10 p-2.5   ${
-                      errors.email ? "border-red-500" : ""
-                    }`}
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                  />
-                  {/* {errors.email && (
+                    {/* Email */}
+                    <div className="relative">
+                      <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                        <img
+                          src={email}
+                          className=" text-secondary items-center"
+                        />
+                      </div>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className={` border text-sm rounded-2xl shadow-md  block w-full ps-10 p-2.5   ${
+                          errors.email ? "border-red-500" : ""
+                        }`}
+                        placeholder="Email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                      />
+                      {/* {errors.email && (
                  <p className="text-red-500 text-xs mt-1">{errors.email}</p>
                )} */}
-                  <img
-                    src={verify}
-                    alt=""
-                    className="absolute right-3 top-4 visible sm:invisible "
-                  />
-                </div>
+                      <img
+                        src={verify}
+                        alt=""
+                        className="absolute right-3 top-4 visible sm:invisible "
+                      />
+                    </div>
 
-                {/* Password */}
-                <div className="relative">
-                  <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <img src={lock} className=" text-secondary items-center" />
+                    {/* Password */}
+                    <div className="relative">
+                      <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                        <img
+                          src={lock}
+                          className=" text-secondary items-center"
+                        />
+                      </div>
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        id="password"
+                        className={` border text-sm rounded-2xl shadow-md   block w-full ps-10 p-2.5 ${
+                          errors.password ? "border-red-500" : ""
+                        }`}
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                      />
+                      <img
+                        src={showPassword ? eyeOn : eyeOff}
+                        alt=""
+                        className="absolute right-3 sm:right-3 top-3 cursor-pointer"
+                        onClick={togglePassword}
+                      />
+                    </div>
+
+                    {/* Confirm Password */}
+                    <div className="relative">
+                      <img
+                        src={lock}
+                        className="absolute top-3 left-3 text-secondary items-center"
+                      />
+                      <input
+                        type={showConfirmPassword ? "text" : "password"}
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        placeholder="Confirm Password"
+                        className={`border rounded-2xl shadow-md block w-full ps-10 p-2.5 ${
+                          errors.confirmPassword ? "border-red-500" : ""
+                        }`}
+                        value={formData.confirmPassword}
+                        onChange={handleInputChange}
+                      />
+                      <img
+                        src={showConfirmPassword ? eyeOn : eyeOff}
+                        alt=""
+                        className="absolute right-3 top-3 cursor-pointer"
+                        onClick={toggleConfirmPassword}
+                      />
+                    </div>
+
+                    {/* Terms and Register Button */}
+                    <div className="flex items-center space-x-2">
+                      <input
+                        type="checkbox"
+                        name="terms"
+                        id="terms"
+                        checked={formData.terms}
+                        onChange={handleInputChange}
+                        className="w-5 h-5"
+                      />
+
+                      <label htmlFor="terms" className="text-sm">
+                        By signing up, you agree to our{" "}
+                        <a href="#" className="text-blue hover:underline">
+                          [Terms of Service]
+                        </a>{" "}
+                        and{" "}
+                        <a href="#" className="text-blue hover:underline">
+                          [Privacy Policy]
+                        </a>
+                        .
+                      </label>
+                    </div>
+                    {errors.terms && (
+                      <p className="text-red-500 text-xs">{errors.terms}</p>
+                    )}
+                  </>
+                ) : (
+                  <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                      <img
+                        src={lock}
+                        className=" text-secondary items-center"
+                      />
+                    </div>
+                    <input
+                      type="text"
+                      name="code"
+                      id="email"
+                      required
+                      className={` border text-sm rounded-2xl shadow-md  block w-[463px] ps-10 p-2.5   ${
+                        errors.email ? "border-red-500" : ""
+                      }`}
+                      placeholder="Enter your code"
+                      value={code}
+                      onChange={(e) => setCode(e.target.value)}
+                    />
+                    {/* {errors.email && (
+                 <p className="text-red-500 text-xs mt-1">{errors.email}</p>
+               )} */}
                   </div>
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    id="password"
-                    className={` border text-sm rounded-2xl shadow-md   block w-full ps-10 p-2.5 ${
-                      errors.password ? "border-red-500" : ""
-                    }`}
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                  />
-                  <img
-                    src={showPassword ? eyeOn : eyeOff}
-                    alt=""
-                    className="absolute right-3 sm:right-3 top-3 cursor-pointer"
-                    onClick={togglePassword}
-                  />
-                </div>
-
-                {/* Confirm Password */}
-                <div className="relative">
-                  <img
-                    src={lock}
-                    className="absolute top-3 left-3 text-secondary items-center"
-                  />
-                  <input
-                    type={showConfirmPassword ? "text" : "password"}
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    placeholder="Confirm Password"
-                    className={`border rounded-2xl shadow-md block w-full ps-10 p-2.5 ${
-                      errors.confirmPassword ? "border-red-500" : ""
-                    }`}
-                    value={formData.confirmPassword}
-                    onChange={handleInputChange}
-                  />
-                  <img
-                    src={showConfirmPassword ? eyeOn : eyeOff}
-                    alt=""
-                    className="absolute right-3 top-3 cursor-pointer"
-                    onClick={toggleConfirmPassword}
-                  />
-                </div>
-
-                {/* Terms and Register Button */}
-                <div className="flex items-center space-x-2">
-                  <input
-                    type="checkbox"
-                    name="terms"
-                    id="terms"
-                    checked={formData.terms}
-                    onChange={handleInputChange}
-                    className="w-5 h-5"
-                  />
-
-                  <label htmlFor="terms" className="text-sm">
-                    By signing up, you agree to our{" "}
-                    <a href="#" className="text-blue hover:underline">
-                      [Terms of Service]
-                    </a>{" "}
-                    and{" "}
-                    <a href="#" className="text-blue hover:underline">
-                      [Privacy Policy]
-                    </a>
-                    .
-                  </label>
-                </div>
-                {errors.terms && (
-                  <p className="text-red-500 text-xs">{errors.terms}</p>
                 )}
-              </>
-            ) : (
-              <div className="relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                  <img src={lock} className=" text-secondary items-center" />
-                </div>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className={` border text-sm rounded-2xl shadow-md  block w-[463px] ps-10 p-2.5   ${
-                    errors.email ? "border-red-500" : ""
-                  }`}
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
-                {/* {errors.email && (
-                 <p className="text-red-500 text-xs mt-1">{errors.email}</p>
-               )} */}
-              </div>
-            )}
 
-            <button className="w-full py-2 bg-primary text-white rounded-3xl shadow-md hover:bg-red-600">
-              {verifyEmail ? "Register" : "Verify"}
-            </button>
-            {submitError && (
-              <p className="text-red-500 text-center mt-4">{submitError}</p>
-            )}
+                <button className="w-full py-2 bg-primary text-white rounded-3xl shadow-md hover:bg-red-600">
+                  {verifyEmail ? "Register" : "Verify"}
+                </button>
+                {submitError && (
+                  <p className="text-red-500 text-center mt-4">{submitError}</p>
+                )}
 
-            <p className="text-center mt-4">
-              Already have an account?{" "}
-              <a href="#" className="text-blue hover:underline">
-                Login
-              </a>
-            </p>
-          </form>
-        </div>
+                <p className="text-center mt-4">
+                  Already have an account?{" "}
+                  <a href="#" className="text-blue hover:underline">
+                    Login
+                  </a>
+                </p>
+              </form>
+            </>
+          </div>
+        ) : (
+          <Loader />
+        )}
       </div>
     </div>
   );
---
+++ src/ui/Spinner.jsx
@@ -0,0 +1,4 @@
+// Loader.js
+const Loader = () => <div className="loader"></div>;
+
+export default Loader;
